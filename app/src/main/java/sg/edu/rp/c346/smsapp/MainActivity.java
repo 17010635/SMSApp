f78b66035277845a83b1163a736f3481849db88b
17010635 <17010635@myrp.edu.sg>
2018-08-15 11:58:52 +0800

    comment

--- app/src/main/java/sg/edu/rp/c346/smsapp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/smsapp/MainActivity.java
@@ -2,14 +2,17 @@ package sg.edu.rp.c346.smsapp;
 
 import android.Manifest;
 import android.content.BroadcastReceiver;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
+import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,7 +20,7 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     EditText etTo, etMsg;
     Button btnSend, btnSendVIAMsg;
-
+    String to, msg;
     BroadcastReceiver br = new MessageReceiver();
 
     @Override
@@ -31,14 +34,29 @@ public class MainActivity extends AppCompatActivity {
         btnSend = findViewById(R.id.buttonSend);
         btnSendVIAMsg = findViewById(R.id.buttonSendMsg);
 
+        to = etTo.getText().toString();
+        msg = etMsg.getText().toString();
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SmsManager smsManager = SmsManager.getDefault();
-                smsManager.sendTextMessage(etTo.getText().toString(), null, etMsg.getText().toString(),null ,null);
+                smsManager.sendTextMessage(to, null, msg,null ,null);
             }
         });
 
+        btnSendVIAMsg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("uuu", "uuu");
+                Uri smsUri = Uri.parse("tel:" + to);
+                Intent intent = new Intent(Intent.ACTION_VIEW, smsUri);
+                intent.putExtra("address", to);
+                intent.putExtra("sms_body", msg);
+                intent.setType("vnd.android-dir/mms-sms");//here setType will set the previous data null.
+                startActivity(intent);
+
+            }
+        });
         IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
         filter.addAction("android.provider.Telephony.SMS_RECEIVED");
         this.registerReceiver(br,filter);
